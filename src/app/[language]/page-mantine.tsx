b1bc728bb977e18a81db1235398942c07d543cf7
NomadNiko <123768788+NomadNiko@users.noreply.github.com>
2025-04-17 10:50:32 +0000

    fix: formatting with prettier

--- src/app/[language]/page-mantine.tsx
+++ src/app/[language]/page-mantine.tsx
@@ -1,13 +1,6 @@
 "use client";
 import { useTranslation } from "@/services/i18n/client";
-import {
-  Container,
-  Stack,
-  Box,
-  Title,
-  Text,
-  Space,
-} from "@mantine/core";
+import { Container, Stack, Box, Title, Text, Space } from "@mantine/core";
 import { useEffect } from "react";
 import { Trans } from "react-i18next/TransWithoutContext";
 
@@ -20,10 +13,10 @@ export default function HomeMantine() {
     const script = document.createElement("script");
     script.src = "https://www.viator.com/orion/partner/widget.js";
     script.async = true;
-    
+
     // Append to document
     document.body.appendChild(script);
-    
+
     // Cleanup function to remove script when component unmounts
     return () => {
       if (document.body.contains(script)) {
@@ -38,44 +31,43 @@ export default function HomeMantine() {
         <Title order={2} data-testid="home-title">
           {t("title")}
         </Title>
-        
+
         <Text>
           <Trans i18nKey={`description`} t={t} />
         </Text>
-        
+
         <Space h="md" />
-        
+
         {/* Viator Widget 1 */}
-        <Box 
+        <Box
           className="viator-widget-container"
           style={{ width: "100%" }}
-          data-vi-partner-id="P00245916" 
+          data-vi-partner-id="P00245916"
           data-vi-widget-ref="W-fb2f650c-8526-4f1d-a790-9e348f4e4852"
         />
-        
+
         <Space h="md" />
-        
+
         {/* Viator Widget 2 */}
-        <Box 
+        <Box
           className="viator-widget-container"
           style={{ width: "100%" }}
-          data-vi-partner-id="P00245916" 
+          data-vi-partner-id="P00245916"
           data-vi-widget-ref="W-80eaccfc-0796-46be-97c3-a436b63dccc0"
         />
-        
+
         <Space h="md" />
-        
+
         {/* Viator Widget 3 */}
-        <Box 
+        <Box
           className="viator-widget-container"
           style={{ width: "100%" }}
-          data-vi-partner-id="P00245916" 
+          data-vi-partner-id="P00245916"
           data-vi-widget-ref="W-4404ebb4-a70c-47b7-ac87-31d377f81e37"
         />
-        
+
         <Space h="xl" />
       </Stack>
     </Container>
   );
 }
- 
